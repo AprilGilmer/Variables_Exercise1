a23696ca7d860948d33636bfe6c9e9d3a2de0379
April Gilmer <aprilgilmer1@gmail.com>
2023-09-08 17:00:23 -0500

    Created Variables Project

---
+++ Variables_Exercise1/Program.cs
@@ -0,0 +1,13 @@
+﻿string name = "Jon";
+int age = 31;
+char middleInitial = 'D';
+bool isOver18 = true;
+double currentTemp = 77.9;
+decimal currentPrice = 29.99m;
+
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age:  {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
